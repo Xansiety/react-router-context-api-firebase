48b8ae16636b06020face2c75f6a3d56daf3e846
xansiety <ferando543@outlook.com>
2023-01-30 19:52:03 -0600

    Integracion con Formik

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,18 @@
+import { Formik } from "formik";
 import { useState } from "react";
 import { loginAuth } from "../config/firebase";
 import { useForm } from "../hooks/useForm";
 import { useRedirectActiveUser } from "../hooks/useRedirectActiveUser";
 import { useUserContext } from "../hooks/useUserContext";
 
-const loginForm = {
-  loginEmail: "",
-  loginPassword: "",
-};
-
-const formValidations = {
-  loginEmail: [(value) => value.includes("@"), "El correo debe de tener una @"],
-  loginPassword: [
-    (value) => value.length >= 6,
-    "El password debe de tener más de 6 letras",
-  ],
-};
-
 const Login = () => {
   const { user } = useUserContext();
   useRedirectActiveUser(user, "/dashboard");
 
-  const [formSubmitted, setFormSubmitted] = useState(false);
-
-  const {
-    loginEmail,
-    loginPassword,
-    onInputChange: onLoginInputChange,
-    isFormValid,
-    loginEmailValid,
-    loginPasswordValid,
-  } = useForm(loginForm, formValidations);
-
-  const onLoginSubmit = async (event) => {
-    event.preventDefault();
-    setFormSubmitted(true);
-    if (!isFormValid) return;
+  const handleSubmitForm = async ({ email, password }) => {
+    console.log(email, password);
     try {
-      const credentials = await loginAuth(loginEmail, loginPassword);
+      const credentials = await loginAuth(email, password);
       console.log(credentials);
     } catch (error) {
       console.error(error);
@@ -47,31 +22,33 @@ const Login = () => {
   return (
     <>
       <h3>Ingreso</h3>
-      <form onSubmit={onLoginSubmit}>
-        <input
-          type="text"
-          placeholder="Ingrese Email"
-          autoComplete="off"
-          name="loginEmail"
-          value={loginEmail}
-          onChange={onLoginInputChange}
-        />
-        {!!loginEmailValid && formSubmitted && loginEmailValid}
 
-        <input
-          type="password"
-          placeholder="Contraseña"
-          autoComplete="off"
-          name="loginPassword"
-          value={loginPassword}
-          onChange={onLoginInputChange}
-        />
-
-        {!!loginPasswordValid && formSubmitted && loginPasswordValid}
-        <div className="d-grid gap-2">
-          <input type="submit" className="btnSubmit" value="Login" />
-        </div>
-      </form>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmitForm}
+      >
+        {({ values, handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="email"
+              value={values.email}
+              onChange={handleChange}
+              name="email"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={values.password}
+              onChange={handleChange}
+              name="password"
+            />
+            <div className="d-grid gap-2">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
